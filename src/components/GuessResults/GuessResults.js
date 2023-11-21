455e88b1599fb29d08c62ed8c0eae0e0ac20e57a
Lucy Macartney <macartney.lucy@gmail.com>
2023-11-21 15:37:39 -0500

    improves variable name and comment

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -6,8 +6,9 @@ import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 function GuessResults({ guesses }) {
   return (
     <div className="guess-results">
-      {range(NUM_OF_GUESSES_ALLOWED).map((el) => (
-        <Guess key={el} guess={guesses[el]} />
+      {/* Exercise 3: generates the grid for the guesses */}
+      {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
+        <Guess key={num} guess={guesses[num]} />
       ))}
     </div>
   );
